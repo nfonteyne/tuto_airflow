a1e5ee9bc21977a0be6289674c3c40d36a9e2cfd
nfonteyne <nathan.fonteyne59@gmail.com>
2024-08-30 22:08:16 +0200

    dag postgres

--- dags/dag_with_postgres_operator.py
+++ dags/dag_with_postgres_operator.py
@@ -7,24 +7,40 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 default_args = {
     'owner': 'nfonteyne',
     'retries': 5,
-    'retry_delay':timedelta(minutes=2)
+    'retry_delay':timedelta(minutes=5)
 }
 with DAG(
-    dag_id='dag_with_postgres_operator_v01',
+    dag_id='dag_with_postgres_operator_V03',
     default_args=default_args,
-    description='This is our first dag that we write',
     start_date=datetime(2024, 8, 25, 2),
     schedule_interval='0 0 * * *'
 ) as dag:
     task1 = PostgresOperator(
         task_id='create_postgres_table',
-        postgres_conn_id="postgres",
+        postgres_conn_id='postgres_localhost',
         sql="""
             create table if not exists dag_runs (
                 dt date,
                 dag_id character varying,
                 primary key (dt, dag_id)
-            )
-        """
+            ) 
+            """
     )
-    task1
+
+    task2 = PostgresOperator(
+        task_id='delete_data_from_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{ dag.dag_id }}'
+            """
+    )
+
+    task3 = PostgresOperator(
+        task_id='insert_into_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
+            """
+    )
+
+    task1 >> task2 >> task3
